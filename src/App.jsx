e2f7ac054ec1884bb49c5f71e0cbb9a95273dc58
ricardo paulo <pauloricardo_wow@hotmail.com>
2023-04-02 12:27:40 -0300

    modificando app para escolher 13 números e separar 10 desses 13

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,39 @@ import "./App.css";
 
 function App() {
   const [numbers, setNumbers] = useState([]);
-  const [odd, setOdd] = useState([]);
-  const [pair, setPair] = useState([]);
+  const [tenNumbers, setTenNumbers] = useState([]);
   const arrayNumbers = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25,
   ];
 
   function getNumber(number) {
-    if (numbers.length < 21) {
+    if (numbers.length < 13) {
       if (numbers.indexOf(number) == -1)
         setNumbers((prevNumbers) => [...prevNumbers, number]);
     } else {
-      alert("Limite Máximo: 21 números");
+      alert("Limite Máximo: 13 números");
       return false;
     }
   }
 
-  function getTwelvesNumber() {
+  function getTenNumbers() {
     const randomNumbers = [...numbers].sort(() => Math.random() - 0.5);
-    let sixPairNumbers = [];
-    let sixOddNumbers = [];
-    randomNumbers.forEach((number) => {
-      if (
-        sixOddNumbers.length < 6 &&
-        number % 2 !== 0 &&
-        !sixOddNumbers.includes(number)
-      ) {
-        sixOddNumbers = [...sixOddNumbers, number];
-      } else if (
-        sixPairNumbers.length < 6 &&
-        number % 2 === 0 &&
-        !sixPairNumbers.includes(number)
-      ) {
-        sixPairNumbers = [...sixPairNumbers, number];
-      }
-    });
-    setPair(sixPairNumbers);
-    setOdd(sixOddNumbers);
+    const tenRandomNumbers = randomNumbers.slice(0, 10);
+    setTenNumbers(tenRandomNumbers);
   }
 
   function deleteNumber() {
-    let newArray = [...numbers];
-    let deletedNumber = Number(newArray.splice(-1, 1).join(""));
+    const newArray = [...numbers];
+    const deletedNumber = Number(newArray.splice(-1, 1));
     setNumbers(newArray);
 
-    if (odd.find((number) => number === deletedNumber)) {
-      const newArrayOdd = [...odd];
+    if (tenNumbers.find((number) => number === deletedNumber)) {
+      const newArrayOdd = [...tenNumbers];
       const filteredArray = newArrayOdd.filter(
         (numbers) => numbers !== deletedNumber
       );
-      setOdd(filteredArray);
-    } else {
-      const newArrayPair = [...pair];
-      const filteredArray = newArrayPair.filter(
-        (numbers) => numbers !== deletedNumber
-      );
-      setPair(filteredArray);
+      setTenNumbers(filteredArray);
     }
   }
 
@@ -67,13 +43,15 @@ function App() {
     <div className="container">
       <div className="selectNumbersContainer">
         {arrayNumbers.map((number) => (
-          <button onClick={() => getNumber(number)}>{number}</button>
+          <button key={number} onClick={() => getNumber(number)}>
+            {number}
+          </button>
         ))}
       </div>
       {numbers.length > 0 && (
         <div className="chosenNumbers">
           {numbers.map((number) => (
-            <p>{number}</p>
+            <p key={number}>{number.toString().padStart(2, "0")}</p>
           ))}
         </div>
       )}
@@ -84,26 +62,18 @@ function App() {
           </button>
         </div>
         <div className="getTwelvesNumbers">
-          <button onClick={getTwelvesNumber}>Selecionar Ímpares e Pares</button>
+          {numbers.length === 13 && (
+            <button onClick={getTenNumbers}>Selecionar 10 Dezenas</button>
+          )}
         </div>
       </div>
       <div className="twelveNumbers">
-        {odd.length > 0 && (
+        {tenNumbers.length > 0 && (
           <div>
-            <h1>Ímpares</h1>
+            <h1>Dez Dezenas</h1>
             <div className="oddNumbers">
-              {odd.map((oddNumber) => (
-                <p>{oddNumber}</p>
-              ))}
-            </div>
-          </div>
-        )}
-        {pair.length > 0 && (
-          <div>
-            <h1>Pares</h1>
-            <div className="pairNumbers">
-              {pair.map((pairNumber) => (
-                <p>{pairNumber}</p>
+              {tenNumbers.map((tenNumbers) => (
+                <p key={tenNumbers}>{tenNumbers.toString().padStart(2, "0")}</p>
               ))}
             </div>
           </div>
